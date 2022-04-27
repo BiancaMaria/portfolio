da8e192713cc7c904a5a03a0dd72e0a56f17f660
@BiancaMaria <bianca.reis@gmz.solutions>
2022-04-27 19:03:20 -0300

    :hammer: Adicionando javascript no projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+function exibeCardProjetos(){
+  console.log('aparece card');
+  let card3 = document.querySelector('#card3');
+  console.log(document.querySelector('#card4'));
+
+  console.log(card3);
+  card3.style.display = 'flex';
+  card4.style.display = 'flex';
+}
