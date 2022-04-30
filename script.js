7d9aac6029f7b4a76e62f18c3a8293acbe93bf50
@BiancaMaria <bianca.reis@gmz.solutions>
2022-04-30 19:07:00 -0300

    :recycle: Reestruturação do layout html e acréscimo da funcionalidade 'Veja Todos'

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const veja = document.getElementById('veja');
 veja.addEventListener('click', viewCardProjects);
 
-function viewCardProjects(){
-  console.log('aparece card');
-  insertedCard();
+function modifyArticle(){
+  const articlePost = document.getElementsByTagName('article')[0];
+  console.log(articlePost);
+  articlePost.classList.toggle('active-posts');
+  articlePost.classList.add('desative_posts');
+
 }
 
-function createCard(){
+function viewCardProjects(){
+  console.log('aparece card');
+  insertCard();
+  modifyArticle();
+}
+
+function createSectionGenerate(){
+  const main = document.getElementsByTagName('main')[0];
+  const section = document.createElement('section');
+  section.className = 'generate-card-projects';
+
+  main.appendChild(section);
+  return section;
+ 
+}
+
+function createCard3(){
   const divCard = document.createElement('div');
   divCard.className = 'card card-projeto3';
   
@@ -46,13 +65,53 @@ function createCard(){
   return divCard;
 }
 
-function insertedCard(){
-  const card =  createCard();
-  const article = document.getElementsByTagName('article')[0];
+function createCard4(){
+  const divCard = document.createElement('div');
+  divCard.className = 'card card-projeto4';
   
-  article.insertAdjacentElement("beforebegin",card);
-  console.log(article);
-  //box.insertAdjacentHTML(html,favourites);
+  const img = document.createElement('img');
+  img.className = 'repository';
+  img.src = './assets/folder.svg';
+  img.alt = 'repositório de projeto';
+  
+  const link = document.createElement('a');
+  link.id = 'component';
+  link.href = 'https://card-component-five.vercel.app/';
+  link.className = 'project-title';
+  link.innerText = 'Projeto 4';
+  
+  const h2 = document.createElement('h2');
+  h2.className = 'subtitle-desc';
+  h2.innerText = 'Projeto desenvolvido a partir dos desafios da Frontend Mentor';
+  
+  const divBolinhaTech = document.createElement('div');
+  divBolinhaTech.className = 'bolinha-tech';
+  
+  const divBolinha = document.createElement('div');
+  divBolinha.className = 'bolinha';
+  divBolinha.id = 'bolinha2'
+
+  const paragrafo = document.createElement('p');
+  paragrafo.innerText = 'CSS';
+  paragrafo.id = 'p-bolinha';
+
+  divCard.appendChild(img);
+  divCard.appendChild(link);
+  divCard.appendChild(h2);
+  divCard.appendChild(divBolinhaTech);
+
+  divBolinhaTech.appendChild(divBolinha);
+  divBolinha.appendChild(paragrafo);
+  return divCard;
+}
+
+function insertCard(){
+  const createSection = createSectionGenerate();
+  const insertCard3 = createCard3();
+  const insertCard4 = createCard4();
+  createSection.appendChild(insertCard3);
+  createSection.appendChild(insertCard4);
+  
 }
 
 
